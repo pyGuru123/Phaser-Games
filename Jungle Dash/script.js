fb4ccc1e5343bf028b0d54b8cfd3aefc023c1914
pyguru123 <pyguru123@gmail.com>
2022-01-20 00:39:08 +0530

    added level data

--- Jungle Dash/script.js
+++ Jungle Dash/script.js
@@ -1,4 +1,7 @@
-const WIDTH=800, HEIGHT=600;
+const WIDTH=864, HEIGHT=576;
+const TILESIZE = 48;
+const ROWS = HEIGHT / TILESIZE;
+const COLS = WIDTH / TILESIZE; 
 
 var config = {
 	type: Phaser.AUTO,
@@ -18,14 +21,45 @@ var config = {
 	}
 }
 
-function preload() {
+var game = new Phaser.Game(config);
 
+function preload() {
+	this.load.image('bg', 'assets/BG1.png');
+
+	for (let i=0; i<9; i++) {
+		image = this.load.image(`tile${i+1}`, `tiles/${i+1}.png`);
+	}
 }
 
 function create() {
+	bg = this.add.image(0, 0, 'bg').setOrigin(0);
 
+	var rect = this.add.rectangle(0, 0, WIDTH, HEIGHT).setOrigin(0);
+	rect.setStrokeStyle(4, 0x1a65ac)
+
+	for (let i=0; i<ROWS; i++) {
+		this.add.line(0, 0, 0, TILESIZE*i, WIDTH, TILESIZE*i, 0x1a65ac).setOrigin(0);
+	}
+	for (let j=0; j<COLS; j++) {
+		this.add.line(0, 0, TILESIZE*j, 0, TILESIZE*j, HEIGHT, 0x1a65ac).setOrigin(0);
+	}
+
+	platforms = this.physics.add.staticGroup();
+	platforms.create(TILESIZE/2, TILESIZE*10-24, 'tile2').setScale(0.4);
+	platforms.create(TILESIZE*2-24, TILESIZE*10-24, 'tile2').setScale(0.4);
+	platforms.create(TILESIZE*3-24, TILESIZE*10-24, 'tile3').setScale(0.4);
+	platforms.create(TILESIZE*3-24, TILESIZE*11-24, 'tile9').setScale(0.4);
+	platforms.create(TILESIZE*3-24, TILESIZE*12-24, 'tile9').setScale(0.4);
+	platforms.create(TILESIZE/2, TILESIZE*11-24, 'tile7').setScale(0.4);
+	platforms.create(TILESIZE/2, TILESIZE*12-24, 'tile7').setScale(0.4);
+	platforms.create(TILESIZE*2-24, TILESIZE*11-24, 'tile7').setScale(0.4);
+	platforms.create(TILESIZE*2-24, TILESIZE*12-24, 'tile7').setScale(0.4);
 }
 
 function update() {
-	
+
+}
+
+function drawGrid() {
+
 }
